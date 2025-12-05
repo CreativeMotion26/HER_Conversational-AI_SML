17a72e42c84205c4e807da61aef7a890ceb34f88
CreativeMotion <kjm4540@gmail.com>
2025-12-05 17:05:12 +1100

    update train set

--- SML Chatbot/train_lora.py
+++ SML Chatbot/train_lora.py
@@ -76,5 +76,5 @@ def train(data):
     
 train([
     {"text": "Human: Hey, how are you today?\nAI Friend: Hey there! 😊 I'm doing great, thanks for asking! How about you? What's been going on in your life?\n"},
-    {"text": "Human: I'm feeling a bit stressed today\nAI Friend: Oh no, I'm sorry to hear that! 😔 Stress can be really tough. Want to talk about what's bothering you? I'm here to listen!\n"}
+    {"text": "Human: I'm feeling a bit stressed\nAI Friend: Oh no, I'm sorry to hear that! 😔 Stress can be really tough. Want to talk about what's bothering you? I'm here to listen!\n"}
 ])
